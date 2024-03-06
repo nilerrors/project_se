0890469976e805540b3a041f2ef9aeee9ed2b7a4
nilerrors <enayat.sabawoon@outlook.com>
2024-03-06 11:42:31 +0100

    add MINGW _assert, to DesignByContract.h to support windows

--- src/DesignByContract.h
+++ src/DesignByContract.h
@@ -8,8 +8,20 @@
 
 #include <assert.h>
 
+#ifdef __MINGW32__
+#define REQUIRE(assertion, what) \
+	if (!(assertion)) _assert (what, __FILE__, __LINE__)
+#else
 #define REQUIRE(assertion, what) \
 	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+#endif
 
+
+#ifdef __MINGW32__
+#define ENSURE(assertion, what) \
+	if (!(assertion)) _assert (what, __FILE__, __LINE__)
+#else
 #define ENSURE(assertion, what) \
 	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+#endif
+
