610f2253b9467926ae3be87efd1edfe49b9c6957
nilerrors <enayat.sabawoon@outlook.com>
2024-03-17 22:50:39 +0100

    add docs to utils

--- src/utils.h
+++ src/utils.h
@@ -16,16 +16,48 @@
 
 #include <iostream>
 
+/**
+ * \brief Checks if a directory exists
+ * @param dirname
+ * @return true if the directory exists
+ */
 bool DirectoryExists(const std::string dirname);
 
+/**
+ * \brief Checks if a file exists
+ * @param dirname
+ * @return true if the file exists
+ */
 bool FileExists(const std::string dirname);
 
+/**
+ * \brief Checks if a file is empty
+ * @param filename
+ * @return true if the file is empty
+ */
 bool FileIsEmpty(const std::string filename);
 
+/**
+ * \brief Compares two files
+ * @param leftFileName
+ * @param rightFileName
+ * @return true if the files are the same
+ */
 bool FileCompare(const std::string leftFileName, const std::string rightFileName);
 
+/**
+ * \brief Checks if a string is a number
+ * @param s
+ * @return true if the string is a number
+ */
 bool is_number(const std::string& s);
 
+/**
+ * \brief Generates a file name
+ * @param prefix
+ * @param extension
+ * @return the generated file name
+ */
 std::string GenerateFileName(const std::string &prefix, const std::string &extension);
 
 #endif //PROJECTTITLE_UTILS_H
